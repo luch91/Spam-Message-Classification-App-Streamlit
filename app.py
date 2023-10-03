d0ce55bac2d6e7fe18b4367593e9ce3e563bb83b
luch91 <61505908+luch91@users.noreply.github.com>
2023-10-03 10:29:30 +0100

    resolved streamlit bug

--- app.py
+++ app.py
@@ -2,8 +2,26 @@ import streamlit as st
 import pickle
 
 # loading the trained model
-model = pickle.load(open("model.pkl", "rb"))
+model = pickle.load(open("C:\\Users\\user\\Desktop\\Spam Classifier\\Spam-Message-Classification-App-Streamlit\\model.pkl", "rb"))
 
 # create title
 
 st.title("Predict if message is Spam or not")
+
+message = st.text_input("Enter your message")
+
+submit = st.button("Predict")
+
+if submit:
+    prediction = model.predict([message])
+
+    print(prediction)
+    st.write(prediction)
+
+    if prediction[0] == "spam":
+        st.warning("This message is spam")
+    else:
+        st.success("This message is Legit(HAM)")
+        
+st.balloons()
+
